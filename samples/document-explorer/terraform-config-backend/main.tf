c14a71a92c83ba21c6caac698ccb05f30a90c2d2
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-11-25 16:40:11 -0800

    feat: v1.0.0 and amd64 as the default
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- samples/document-explorer/terraform-config-backend/main.tf
+++ samples/document-explorer/terraform-config-backend/main.tf
@@ -1,11 +1,8 @@
 module "genai_doc_ingestion" {
   #checkov:skip=CKV_TF_1:Terraform registry has no ability to use a commit hash
-  ## TODO: Update to a Terraform registry version once PR is complete
-  #source             = "aws-ia/genai-document-ingestion-rag/aws"
-  #version            = "0.0.4"
-  #source             = "github.com/aws-ia/terraform-aws-genai-document-ingestion-rag//?ref=fix%2Fdocker_build"
-  source             = "github.com/aws-ia/terraform-aws-genai-document-ingestion-rag//?ref=11eb2bac799fd495e46d17f11156eefe5e6d9d71"
+  source             = "aws-ia/genai-document-ingestion-rag/aws"
+  version            = "1.0.0"
   solution_prefix    = "doc-explorer"
-  container_platform = "linux/arm64"
+  container_platform = "linux/amd64"
   force_destroy      = true
 }
